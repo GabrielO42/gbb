62210eae9ad29908a873ea79f5e2d74ccb5e459a
GabrielO42 <gbxz097@gmail.com>
2026-09-22 17:10:13 -0300

    gb

---
+++ Aluno.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.df.atividade3;
+
+/**
+ *
+ * @author gabriel62947886
+ */
+public class Aluno {
+    public static void main(String[] args) {
+   
+    }
+}
---
+++ Professor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.senac.df.atividade3;
+
+/**
+ *
+ * @author gabriel62947886
+ */
+public class Professor {
+    
+}
